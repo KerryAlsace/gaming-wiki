72651d47203efaf5200c2142eb1a80088c649512
KerryAlsace <kerrydougherty@gmail.com>
2016-12-05 16:38:16 -0500

    build out game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,5 +4,46 @@ class GamesController < ApplicationController
     games = Game.all
     render json: games
   end
-  
+
+  def show
+    game = Game.find(params[:id])
+    render json: game
+  end
+
+  def new
+    game = Game.new
+  end
+
+  def create
+    game = current_user.games.build(game_params)
+    if game.save
+      render json: { status: 'ok' }
+    else
+      render json: { errors: game.errors.full_messages }, status: :unprocessable_entitiy
+    end
+  end
+
+  def update
+    game = Game.find(params[:id])
+    if game.update(game_params)
+      render json: { status: 'ok' }
+    else
+      render json: { errors: game.errors.full_messages }, status: :unprocessable_entitiy
+    end
+  end
+
+  def destroy
+    game = Game.find(params[:id])
+    if game.destroy
+      render json: { status: 'ok' }
+    else
+      render json: { errors: game.errors.full_messages }, status: :unprocessable_entitiy
+    end
+  end
+
+  private
+    def game_params
+      params.require(:game).permit(:user, :studio, :name, :content, :ratings, :reviews)
+    end
+
 end
